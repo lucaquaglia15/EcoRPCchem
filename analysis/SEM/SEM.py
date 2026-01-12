91719a9669097b66d730faba52895e6df2be2628
Luca Quaglia <luca.quaglia@cern.ch>
2026-01-12 18:16:19 +0100

    Primitive peak detection algorithm implemented, still to be refined

--- analysis/SEM/SEM.py
+++ analysis/SEM/SEM.py
@@ -1,12 +1,18 @@
-import csv
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
-import scipy as s
+import pandas as pd
+from peakutils import indexes
+from scipy.signal import find_peaks as fp
 
 def main():
 
     #If true -> debug printouts enabled
     debug = False
+
+    ##############################
+    # Load energy emission lines #
+    ##############################
 
     #Path to x-ray emission lines table
     pathEmission = "/home/luca/cernbox/marieCurie/EcoRPCchem/data/xRayEmissions.txt"
@@ -30,7 +36,7 @@ def main():
            #Add all emission energy to a temporary list
             for temp in element:
                 if temp != '—':
-                    tempEn.append(float(temp))
+                    tempEn.append(float(temp)*1e-3)
 
             #Add list to dictionary, key is element name
             emissionDict[key] = tempEn.copy()
@@ -41,11 +47,12 @@ def main():
     if debug:
         print(emissionDict)
 
-    #Path to spectrum csv file
-    path = "/home/luca/cernbox/marieCurie/EcoRPCchem/data/SEM_measurements_December2025_w49/csvSpectra/S1_G1/Area 1/Full Area 1_1.csv"
+    #############################
+    # Load spectra from samples #
+    #############################
 
-    #Load spectrum into np array
-    spectrum = np.genfromtxt(path, delimiter = ',')
+    #Path to spectrum csv file to draw histo
+    path = "/home/luca/cernbox/marieCurie/EcoRPCchem/data/SEM_measurements_December2025_w49/csvSpectra/S1_G1/Area 1/Full Area 1_1.csv"
 
     energy, counts = np.genfromtxt(path,delimiter=',',unpack=True)
 
@@ -53,12 +60,43 @@ def main():
         print("Energy:",energy)
         print("Counts:",counts)
 
+    #Load spectrum to find peaks
+    spectrum = pd.read_csv(path, delimiter = ',',index_col=0)
+    spectrum.columns = ["Counts"]
+    spectrum.index.names = ['Energy']
+    
+    if debug:
+        print(spectrum)
+
+    #peak utils peak find parameters
+    mdist = 1
+    thres_ = 0.03
+
+    p1 = indexes(spectrum.Counts.values, min_dist=mdist,thres=thres_)
+    print(spectrum.iloc[p1])
+
+    #Raw spectrum
+    #spectrum.plot()
+
+    #With peaks
+    spectrum.plot()
+    
+    sns.scatterplot(data=spectrum.iloc[p1],
+                    x = "Energy",
+                    y = "Counts",
+                    color = "red")
+    
+    plt.show()
+
+    """
+    #Draw histo
     plt.hist(energy,bins=4096,weights=counts,histtype='step')
 
     plt.xlabel("Energy [keV]")
     plt.ylabel("Counts")
     plt.yscale("log")  # Convert y-axis to logarithmic scale
     plt.show()
+    """
 
 if __name__ == "__main__":
     main()
