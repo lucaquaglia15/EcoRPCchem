23b8bf631dcf87cc0fc66bd9c08c38cb36b5f5a1
Luca Quaglia <luca.quaglia@cern.ch>
2026-04-12 16:16:18 +0200

    Added shell script to run multiple analyses on different samples

---
+++ analysis/SEM/runSemAnalysis.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 SEM.py "~/marieCurie/EcoRPCchem/data/bakelite/S7/S7_B0/csv_spectra_S7_B0/Area 1/EDS Spot 1_1.csv" 0
+python3 SEM.py "~/marieCurie/EcoRPCchem/data/bakelite/S7/S7_B0/csv_spectra_S7_B0/Area 1/EDS Spot 2_1.csv" 1
+python3 SEM.py "~/marieCurie/EcoRPCchem/data/bakelite/S7/S7_B0/csv_spectra_S7_B0/Area 1/EDS Spot 3_1.csv" 0
